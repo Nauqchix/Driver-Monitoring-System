f56398458317a9df2eedd9ebf59c936d9f16affc
Quan <quannguyen2005cqt@gmail.com>
2026-03-01 17:17:42 +0700

    Bootloader + App demo

---
+++ DriverMonitoringBoot/Bootloader_test/Core/Inc/application.h
@@ -0,0 +1,14 @@
+/*
+ * application.h
+ *
+ *  Created on: Mar 1, 2026
+ *      Author: Nguyen Hoang Quan
+ */
+
+#ifndef INC_APPLICATION_H_
+#define INC_APPLICATION_H_
+
+void jump_to_app(void);
+int app_is_valid(void);
+
+#endif /* INC_APPLICATION_H_ */
